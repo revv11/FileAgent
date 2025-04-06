37364bf41fd6122e97be0eca8ad7224dd906f901
revv11 <anand.utkarsh18@gmail.com>
2025-04-07 01:36:42 +0530

    name changed

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
   return (
     <div className="flex justify-between bg-white shadow-sm p-5 border-b">
       <Link href="/dashboard" className="text-2xl">
-        Chat to <span className="text-indigo-600">PDF</span>
+        File<span className="text-indigo-600">Agent</span>
       </Link>
 
       <SignedIn>
